02d919e20e1545fc83f8369591155ccb7a4371d5
mike <dinhmq1@gmail.com>
2018-01-30 22:48:28 -0600

    added on and off links inside each component

--- js/LightSwitchOff.jsx
+++ js/LightSwitchOff.jsx
@@ -6,6 +6,7 @@ const LightSwitchOff = () => (
   <div className="light-switch-off">
     <div className="gray-box">
       <h1 className="off">Light Switch OFF</h1>
+      <Link to="/light-switch-on">Turn lightswitch ON</Link>
       <Link to="/">Take me home</Link>
     </div>
   </div>
--- js/LightSwitchOn.jsx
+++ js/LightSwitchOn.jsx
@@ -6,6 +6,7 @@ const LightSwitchOn = () => (
   <div className="light-switch-on">
     <div className="gray-box">
       <h1 className="on">Light Switch ON</h1>
+      <Link to="/light-switch-off">Turn lightswitch OFF</Link>
       <Link to="/">Take me home</Link>
     </div>
   </div>
